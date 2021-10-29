ff9349ba92e1df0018b44d514f1007277d657f17
luster1031 <61460836+luster1031@users.noreply.github.com>
2021-10-29 23:01:38 +0900

    시간초과

---
+++ 18258_큐2/첫번째 시도_실패.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include <queue>
+#include<string>
+using namespace std;
+int main() {
+	queue<int> input;
+	int N, number;
+	string str;
+
+	cin >> N;
+	while (N--) {
+		cin >> str;
+		if (str == "push") {
+			cin >> number;
+			input.push(number);
+		}
+		else if (str == "pop") {
+			if (input.empty()) cout << "-1\n"; 
+			else {
+				cout << input.front() << "\n";
+				input.pop();
+			}
+		}
+		else if (str == "size")
+			cout << input.size() << "\n";
+		else if (str == "empty")
+			if (input.empty())	cout << "1\n";
+			else cout << "0\n";
+		else if (str == "front") {
+			if (input.empty()) cout << "-1\n";
+			else  cout << input.front() << "\n";
+		}
+		else
+			if (input.empty())
+				cout << "-1\n";
+			else cout << input.back() << "\n";
+	}
+
+	return 0;
+}
